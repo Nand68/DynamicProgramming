0b13ba5b222f939ec45f80dbb29986bdabf155c8
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2025-01-07 15:24:00 +0530

    Update Check if there exists a subsequence with sum K.java

--- Check if there exists a subsequence with sum K.java
+++ Check if there exists a subsequence with sum K.java
@@ -107,7 +107,45 @@ class Solution {
       return (dp[N-1][K] == 1);
   }
 }
- 
+
+// space optimization
+class Solution {
+  public static boolean checkSubsequenceSum(int N, int[] arr, int K) {
+    //   int[][] dp = new int[N][K+1];
+    int[] prev = new int[K+1];
+    //   for(int i = 0; i < N; i++){
+    //       dp[i][0] = 1;
+    //   }
+    prev[0] = 1;
+      for(int i = 1; i <= K; i++){
+          if(i == arr[0]){
+             prev[i] = 1;
+          }
+          else{
+             prev[i] = 2;
+          }
+      }
+      for(int i = 1;i < N; i++){
+          int[] cur = new int[K+1];
+          cur[0] = 1;
+          for(int j = 1; j <= K; j++){
+            int pick = 2;
+                if ( arr[i] <= j){
+                    pick = prev[j-arr[i]]; //solve(arr,j-arr[i],i-1,dp);
+                        if (pick == 1){
+                            cur[j] = 1;
+                            continue;
+                        }
+                }
+                int notpick = prev[j]; //solve(arr,j,i-1,dp);
+                cur[j] = notpick;
+                
+          }
+          prev = cur;
+      }
+      return (prev[K] == 1);
+  }
+}
      
      
   
