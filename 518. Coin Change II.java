3e4101f801821fe254a6aa63ed85e058c870d1fe
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2025-01-09 20:00:54 +0530

    Update 518. Coin Change II.java

--- 518. Coin Change II.java
+++ 518. Coin Change II.java
@@ -58,3 +58,46 @@ class Solution {
         return dp[index][N];
     }
 }
+
+// bottom up approach 
+
+
+class Solution {
+    public int cutRod(int[] price) {
+        int n = price.length;
+        int[][] dp = new int[n][n+1];
+        // for(int i = 0; i < n; i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        for(int i = 0; i < n+1; i++){
+            dp[0][i] = i*price[0];
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n+1; j++){
+                    int pick = 0;
+                    int curlen = i + 1;
+                    if ( curlen <= j){
+                        pick = price[i] + dp[i][j-curlen]; // solve(j-curlen,price,i,dp);
+                    }
+                    int notpick = dp[i-1][j]; //solve(j,price,i-1,dp);
+                     dp[i][j] = Math.max(pick,notpick);
+            }
+        }
+        return dp[n-1][n];
+    }
+    // public int solve(int N,int[] price ,int index, int[][] dp){
+    //     if(index == 0){
+    //         dp[index][N] = N*price[index];
+    //         return N*price[index];
+    //     }
+    //     int pick = 0;
+    //     int curlen = index + 1;
+    //     if ( curlen <= N){
+    //         pick = price[index] + solve(N-curlen,price,index,dp);
+    //     }
+    //     int notpick = solve(N,price,index-1,dp);
+    //      dp[index][N] = Math.max(pick,notpick);
+        
+    //     return dp[index][N];
+    // }
+}
